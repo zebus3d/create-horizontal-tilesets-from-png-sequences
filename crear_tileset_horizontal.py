1966e8e1e72ffd7f2dce697acded09853b8d6136
zebus3d <zebus3dream@gmail.com>
2019-11-29 19:07:20 +0100

    small updates

--- crear_tileset_horizontal.py
+++ crear_tileset_horizontal.py
@@ -10,6 +10,20 @@ from PIL import Image
 # This script must be executed next to the pngs sequence.
 ##########################################################################
 
+'''
+2019 Jorge Hernandez - Melendez
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
 # Settings:
 input_seq_format = "png"  # all png in current dir
 type_input_img = "RGBA"
