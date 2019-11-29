bc54b6ed1a7cbf8cc1f8bb7be8a57bf72970754c
zebus3d <zebus3dream@gmail.com>
2019-11-29 18:35:02 +0100

    agregando examples

--- crear_tileset_horizontal.py
+++ crear_tileset_horizontal.py
@@ -1,25 +1,25 @@
-## crear tileset horizontal desde una secuencia de pngs:
+##########################################################################
+# Crear un tileset horizontal a partir de una secuencia de imagenes .pngs:
 
 import sys, os, re
 from PIL import Image
 
-sequence_format = "png"
-
-outfile = "tileset.png"
+input_seq_format = "png"
+type_input_img = "RGBA"
+type_output_img = "RGBA"
+output_tileset_file = "tileset.png"
 out_format = "PNG"
 
 # si existe ya el tileset lo borro:
-if os.path.exists(outfile):
-    os.remove(outfile)
-
-# images = [Image.open(x) for x in ['Test1.jpg', 'Test2.jpg', 'Test3.jpg']]
-# images = [Image.open(seq_img) for seq_img in os.listdir('.') if re.match(r'.*\.' + sequence_format, seq_img)]
+if os.path.exists(output_tileset_file):
+    os.remove(output_tileset_file)
 
 images = []
 for seq_img in os.listdir('.'):
-    if re.match(r'.*\.' + sequence_format, seq_img):
+    if re.match(r'.*\.' + input_seq_format, seq_img):
         im = Image.open(seq_img)
-        im = im.convert('RGBA')
+        if type_input_img == "RGBA":
+            im = im.convert('RGBA')
         images.append(im)
 
 widths, heights = zip(*(i.size for i in images))
@@ -27,7 +27,10 @@ widths, heights = zip(*(i.size for i in images))
 total_width = sum(widths)
 max_height = max(heights)
 
-new_im = Image.new('RGBA', (total_width, max_height))
+if type_output_img == "RGBA":
+    new_im = Image.new('RGBA', (total_width, max_height))
+else:
+    new_im = Image.new('RGB', (total_width, max_height))
 
 x_offset = 0
 
@@ -35,5 +38,4 @@ for im in images:
     new_im.paste(im, (x_offset, 0))
     x_offset += im.size[0]
 
-# new_im.save('tileset.jpg')
-new_im.save(outfile, out_format)
+new_im.save(output_tileset_file, out_format)
